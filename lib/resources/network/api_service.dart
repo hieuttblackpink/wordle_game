f412ff5bd76b2aec18137356fb41d15d6840a070
hieuttblackpink <trantronghieu369963@gmail.com>
2024-11-27 15:01:49 +0700

    Network: add railway server

--- lib/resources/network/api_service.dart
+++ lib/resources/network/api_service.dart
@@ -1,4 +1,5 @@
 class ApiService {
-  static const String _baseUrl = "http://127.0.0.1:8000/"; //"https://wordle.votee.dev:8000/";
+  static const String _baseUrl =
+      "https://hieutt-wordle-server.up.railway.app/"; //"http://127.0.0.1:8000/"; //"https://wordle.votee.dev:8000/";
   static String get baseUrl => _baseUrl;
 }
